4c222e90e1fddaea4af9c019e6d6a4cbd27ac0f7
Razu Ahmed <48282589+razuhmd@users.noreply.github.com>
2021-08-31 14:43:41 +0200

    properties and constructor

---
+++ MyFirstGitHubClassRoomConsoleApp/Demo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstGitHubClassRoomConsoleApp
+{
+    public class Demo
+    {
+        private int id;
+        private string name;
+        public Demo(int Id, string Name) 
+        {
+            id = Id;
+            name = Name;
+        }
+    }
+}
